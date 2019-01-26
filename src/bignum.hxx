eba6d1b3dabcc88a5e596744ab7f8ab9f22ad460
Malandain Timothe <timothe.malandain@epita.fr>
2019-01-26 16:36:36 +0100

    bignum.hxx : only addition operator

--- src/bignum.hxx
+++ src/bignum.hxx
@@ -1,20 +1,26 @@
+#pragma once
+
+#include "bignum.hh"
 
 namespace bistro
 {
     template <typename T>
-    BigNum::BigNum(std::size_t base)
-        : base_(base)
-        , set_ = digits_t 
-    {}
-
-    template <typename T, typename Base>
-    BigNum::BigNum(std::istream& in, const Base& b)
+    BigNum<T>::BigNum(std::size_t base)
     {
-        std::char token;
-        std::char tmp = in >> token;
-        indet_t n = 0;
+        base_ = base;
+        set_ = digits_t();
+        sign_ = true;
+    }
+
+    template <typename T>
+    template <typename Base>
+    BigNum<T>::BigNum(std::istream& in, const Base& b)
+    {
+        char token;
+        char tmp = in.get();
+        index_t n = 0;
         if (!b.is_digit(tmp))
-            throw std::length_error(tmp);           //maybe deref char
+            throw std::length_error("Not a digit");           //maybe deref char
         else
         {
             set_digit(n, token);
@@ -28,47 +34,54 @@ namespace bistro
                 }
             }
         }
+        std::reverse(set_.begin(),set_.end()); 
     }
 
     template <typename T>
-    BigNum::self_t BigNum::clone() const
+    typename BigNum<T>::self_t BigNum<T>::clone() const
     {
-        
+        self_t res(base_);
+        res.set_ = set_;
+        res.sign_ = sign_;
+        return res;
     }
 
     template <typename T>
-    BigNum::index_t BigNum::get_num_digits() const
+    typename BigNum<T>::index_t BigNum<T>::get_num_digits() const
     {
-          index_t l = base_.size();
+          index_t l = set_.size();
           return l;
     }
 
     template <typename T>
-    BigNum::digit_t BigNum::get_digit(index_t i) const
+    typename BigNum<T>::digit_t BigNum<T>::get_digit(index_t i) const
     {
         index_t n = get_num_digits();
         if (i > n)
-            throw std::out_of_range(i);
+            throw std::out_of_range("Index out of range");
         else
         {
-            return base_[i];
+            return set_[i];
         }
     }
 
     template <typename T>
-    void BigNum::set_digit(index_t i, digit_t d)
+    void BigNum<T>::set_digit(index_t i, digit_t d)
     {
         if (d >= base_)
-            throw std::invalid_argument(d);
+            throw std::invalid_argument("Invalide argument");
         else
         {
-            base_[i] = d;
-            //strip or add 0
+            while( i > set_.size())
+            {
+                set_.emplace_back(0);
+            }
+            set_[i] = d;
         }
     }
 
     template <typename T>
-    bool BigNum::is_positive() const
+    bool BigNum<T>::is_positive() const
     {
         if (sign_)
             return sign_;
@@ -76,21 +89,50 @@ namespace bistro
     }
 
     template <typename T>
-    void BigNum::set_positive(bool p)
+    void BigNum<T>::set_positive(bool p)
     {
         sign_ = p;
     }
 
-
+    template <typename T>
     template <typename Base>
-    std::ostream& BigNum::print(std::ostream& out, const Base& b) const
+    std::ostream& BigNum<T>::print(std::ostream& out, const Base& b) const
     {
-        for (auto rit = this.rbegin(); rit != this.rend(); ++rit)
-            out << b.get_digit_representation(*rit); 
+        for (auto rit = set_.rbegin(); rit != set_.rend(); ++rit)
+            out << b.get_digit_representation(*rit);
+        return out; 
     }
 
-    template <typename Base>
-    bool BigNum::Boperator>(const self_t& other) const
+    template <typename T>
+    typename BigNum<T>::self_t BigNum<T>::operator+(const self_t& other) const
+    {
+        digits_t num1 = set_;
+        digits_t num2 = other;
+        digits_t result = digits_t();
+        digit_t carried = 0;
+        if (num1.size() < num2.size())
+        {
+            num1 = other;
+            num2 = set_;
+        }
+        for (auto it1 = num1.begin(); it1 < num1.end(); it1++)
+        {
+            for (auto it2 = num2.begin(); it2 < num2.end(); it2++)
+            {
+                auto digit = it1 + it2 + carried;
+                if (digit / base_ > 0)
+                {
+                    carried = digit % base_;
+                    digit = digit / base_;
+                }
+                else
+                    carried = 0;
+            }
+        }
+    }
+
+    template <typename T>
+    bool BigNum<T>::operator>(const self_t& other) const
     {
         if (this.get_num_digits() != other.get_num_digits())
             return false;
@@ -99,16 +141,17 @@ namespace bistro
 
         auto rit = this.rbegin();
         auto rother = other.rbegin();
-        for (; rit < this.rend() || rother < rother.rend; it++ ; rother++)
+        for (; rit < this.rend() || rother < rother.rend; rit++)
         {
             if (*rit > *rother)
                 return true;
+            rother++;
         }
         return false;
     }
 
-    template <typename Base>
-    bool BigNum::operator==(const self_t& other) const
+    template <typename T>
+    bool BigNum<T>::operator==(const self_t& other) const
     {
         if (this.get_num_digits() != other.get_num_digits())
             return false;
@@ -117,17 +160,18 @@ namespace bistro
 
         auto rit = this.rbegin();
         auto rother = other.rbegin();
-        for (; rit < this.rend() || rother < rother.rend; it++ ; rother++)
+        for (; rit < this.rend() || rother < rother.rend; rit++)
         {
             if (*rit != *rother)
                 return false;
+            rother++;
         }
         return true;
     }
 
-    template <typename Base>
-    explicit BigNum::operator bool() const
+    template <typename T>
+    BigNum<T>::operator bool() const
     {
         return this.size() != 0;
     }
-};
+}
